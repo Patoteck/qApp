8ffb24ad8f1dcf30607ed6ce99d9c528972f888d
Patrcio Hernandez <patoteckcomputacion@gmail.com>
2019-03-13 01:25:06 -0300

    add all

--- service-worker.js
+++ service-worker.js
@@ -33,11 +33,13 @@ workbox.routing.registerRoute(
 );
 
 self.addEventListener('push', (event) => {
+  console.log("neeeeee entro aca me meu");
   const title = 'Get Started With Workbox';
   const options = {
     body: event.data.text()
   };
   event.waitUntil(self.registration.showNotification(title, options));
+  
 });
 
 
