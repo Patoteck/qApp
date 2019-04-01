71c4c53b06208efb2d2642b9b073eaf9685bda70
Patrcio Hernandez <patoteckcomputacion@gmail.com>
2019-04-01 18:16:25 -0300

    add

--- service-worker.js
+++ service-worker.js
@@ -72,4 +72,46 @@ self.addEventListener('push', e => {
 });
 
 
+// Cierra la notificacion
+self.addEventListener('notificationclose', e => {
+    console.log('Notificación cerrada', e);
+});
+
+
+self.addEventListener('notificationclick', e => {
+
+
+    const notificacion = e.notification;
+    const accion = e.action;
+
+
+    console.log({ notificacion, accion });
+    // console.log(notificacion);
+    // console.log(accion);
+
+
+    const respuesta = clients.matchAll()
+    .then( clientes => {
+
+        let cliente = clientes.find( c => {
+            return c.visibilityState === 'visible';
+        });
+
+        if ( cliente !== undefined ) {
+            cliente.navigate( notificacion.data.url );
+            cliente.focus();
+        } else {
+            clients.openWindow( notificacion.data.url );
+        }
+
+        return notificacion.close();
+
+    });
+
+    e.waitUntil( respuesta );
+
+
+});
+
+
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
